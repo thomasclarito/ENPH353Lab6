f8eb2190d321551dd9d88edb4ea879068e62ba49
thomasclarito <43707071+thomasclarito@users.noreply.github.com>
2020-10-30 11:31:13 -0700

    initial commit

---
+++ gazebo_lab06/gazebo_env_lab06.py
@@ -0,0 +1,234 @@
+
+import cv2
+import gym
+import math
+import rospy
+import roslaunch
+import time
+import numpy as np
+
+from cv_bridge import CvBridge, CvBridgeError
+from gym import utils, spaces
+from gym_gazebo.envs import gazebo_env
+from geometry_msgs.msg import Twist
+from std_srvs.srv import Empty
+
+from sensor_msgs.msg import Image
+from time import sleep
+
+from gym.utils import seeding
+
+
+class Gazebo_Lab06_Env(gazebo_env.GazeboEnv):
+
+    def __init__(self):
+        # Launch the simulation with the given launchfile name
+        LAUNCH_FILE = '/home/fizzer/enph353_gym-gazebo/gym_gazebo/envs/ros_ws/src/enph353_lab06/launch/lab06_world.launch'
+        gazebo_env.GazeboEnv.__init__(self, LAUNCH_FILE)
+        self.vel_pub = rospy.Publisher('/cmd_vel', Twist, queue_size=1)
+        self.unpause = rospy.ServiceProxy('/gazebo/unpause_physics', Empty)
+        self.pause = rospy.ServiceProxy('/gazebo/pause_physics', Empty)
+        self.reset_proxy = rospy.ServiceProxy('/gazebo/reset_world',
+                                              Empty)
+
+        self.action_space = spaces.Discrete(3)  # F,L,R
+        self.reward_range = (-np.inf, np.inf)
+        self.episode_history = []
+
+        self._seed()
+
+        self.bridge = CvBridge()
+        self.timeout = 0  # Used to keep track of images with no line detected
+
+        self.lower_blue = np.array([97,  0,   0])
+        self.upper_blue = np.array([150, 255, 255])
+
+    def process_image(self, data):
+        '''
+            @brief Coverts data into a opencv image and displays it
+            @param data : Image data from ROS
+
+            @retval (state, done)
+        '''
+        try:
+            cv_image = self.bridge.imgmsg_to_cv2(data, "bgr8")
+        except CvBridgeError as e:
+            print(e)
+
+        # cv2.imshow("raw", cv_image)
+
+        state = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+        done = False
+
+        # TODO: Analyze the cv_image and compute the state array and
+        # episode termination condition.
+        #
+        # The state array is a list of 10 elements indicating where in the
+        # image the line is:
+        # i.e.
+        #    [1, 0, 0, 0, 0, 0, 0, 0, 0, 0] indicates line is on the left
+        #    [0, 0, 0, 0, 1, 0, 0, 0, 0, 0] indicates line is in the center
+        #
+        # The episode termination condition should be triggered when the line
+        # is not detected for more than 30 frames. In this case set the done
+        # variable to True.
+        #
+        # You can use the self.timeout variable to keep track of which frames
+        # have no line detected.
+
+        # The following code is derived from Lab 3
+
+        height, width, channels = cv_image.shape
+
+        image_hsv = cv2.cvtColor(cv_image, cv2.COLOR_BGR2HSV)
+        
+        image_bgr = cv_image
+        top_image_hsv = image_hsv[0:height-35, 0:width]
+        bottom_image_hsv = image_hsv[height-35:height, 0:width]
+        
+        height_top, width_top, channels_top = top_image_hsv.shape
+        height_bottom, width_bottom, channels_bottom = bottom_image_hsv.shape
+
+        upperBound = np.array([130, 255, 255], np.uint8)
+        lowerBound = np.array([100, 0, 0], np.uint8)
+        
+        bottom_mask = cv2.inRange(bottom_image_hsv, lowerBound, upperBound)
+        top_mask = cv2.inRange(top_image_hsv, lowerBound, upperBound)
+
+        Mt = cv2.moments(top_mask)
+        Mb = cv2.moments(bottom_mask)
+        cv2.imshow("image", bottom_mask)
+        cv2.waitKey(3)
+
+        if (int(Mt["m00"]) != 0):
+            cXt = int(Mt["m10"] / Mt["m00"])
+            #print("Found line!")
+        else:
+            cXt = -1
+            
+            #print("No line found!") 
+        
+        if (int(Mb["m00"]) != 0):
+            cXb = int(Mb["m10"] / Mb["m00"])
+            cYb = int(Mb["m01"] / Mb["m00"])
+            self.timeout = 0
+            #print("Found line!")
+        else:
+            self.timeout += 1
+            cXb = -1
+        
+        num_slices = len(state)
+        cut_length = width/num_slices
+
+        if (cXb != -1):
+            index = cXb // cut_length
+            state[index] = 1 
+
+        top_state = [0]*num_slices
+        
+        if (self.timeout > 30):
+            done = True
+
+        print(state)
+        return state, done
+
+    def _seed(self, seed=None):
+        self.np_random, seed = seeding.np_random(seed)
+        return [seed]
+
+    def step(self, action):
+
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        try:
+            self.unpause()
+        except (rospy.ServiceException) as e:
+            print ("/gazebo/unpause_physics service call failed")
+
+        self.episode_history.append(action)
+
+        vel_cmd = Twist()
+
+        if action == 0:  # FORWARD
+            vel_cmd.linear.x = 0.4
+            vel_cmd.angular.z = 0.0
+        elif action == 1:  # LEFT
+            vel_cmd.linear.x = 0.0
+            vel_cmd.angular.z = 0.5
+        elif action == 2:  # RIGHT
+            vel_cmd.linear.x = 0.0
+            vel_cmd.angular.z = -0.5
+
+        self.vel_pub.publish(vel_cmd)
+
+        data = None
+        while data is None:
+            try:
+                data = rospy.wait_for_message('/pi_camera/image_raw', Image,
+                                              timeout=5)
+            except:
+                pass
+
+        rospy.wait_for_service('/gazebo/pause_physics')
+        try:
+            # resp_pause = pause.call()
+            self.pause()
+        except (rospy.ServiceException) as e:
+            print ("/gazebo/pause_physics service call failed")
+
+        state, done = self.process_image(data)
+
+        # Set the rewards for your action
+        if not done:
+            if action == 0:  # FORWARD
+                reward = 4
+            elif action == 1:  # LEFT
+                reward = 2
+            else:
+                reward = 2  # RIGHT
+        else:
+            reward = -200
+
+        return state, reward, done, {}
+
+    def reset(self):
+
+        print("Episode history: {}".format(self.episode_history))
+        self.episode_history = []
+        print("Resetting simulation...")
+        # Resets the state of the environment and returns an initial
+        # observation.
+        rospy.wait_for_service('/gazebo/reset_simulation')
+        try:
+            # reset_proxy.call()
+            self.reset_proxy()
+        except (rospy.ServiceException) as e:
+            print ("/gazebo/reset_simulation service call failed")
+
+        # Unpause simulation to make observation
+        rospy.wait_for_service('/gazebo/unpause_physics')
+        try:
+            # resp_pause = pause.call()
+            self.unpause()
+        except (rospy.ServiceException) as e:
+            print ("/gazebo/unpause_physics service call failed")
+
+        # read image data
+        data = None
+        while data is None:
+            try:
+                data = rospy.wait_for_message('/pi_camera/image_raw',
+                                              Image, timeout=5)
+            except:
+                pass
+
+        rospy.wait_for_service('/gazebo/pause_physics')
+        try:
+            # resp_pause = pause.call()
+            self.pause()
+        except (rospy.ServiceException) as e:
+            print ("/gazebo/pause_physics service call failed")
+
+        self.timeout = 0
+        state, done = self.process_image(data)
+
+        return state
